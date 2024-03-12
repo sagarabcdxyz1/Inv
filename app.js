8ae83e365785accc82e758117d4f05a0f62d915b
sagarabcdxyz1 <sagarabcdxyz1@gmail.com>
2024-03-12 16:51:00 +0530

    check

--- app.js
+++ app.js
@@ -255,8 +255,8 @@ app.get('/auth/google/callback',
   
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
